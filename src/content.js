f30fb41d2ac6864f2a32dc645b4ab2c1e3a3b5d6
umbrellaj <umbrella.j@gmail.com>
2012-12-07 17:01:54 -0500

    v0.0.5 commit, update link when search box change

--- src/content.js
+++ src/content.js
@@ -1,11 +1,14 @@
 function getKeywords() {
-	var n=document.URL.match(/www\.google\.com\/search\?(.*)/);
+	/*
+	var n=document.URL.match(/www\.google\.com\/search\?(.*)#(.*)/);
+
 	if(n && n.length>1) {
-		var n= n[1].match(/(^|&)q=([^&]*)(&|$)/);
-		return n[2]; 
+		var q= n[-1].match(/(^|&)q=([^&]*)(&|$)/g);
+		return q[2];
 	} else {
 		return "";
-	}
+	}*/
+	return encodeURI(gbqfq.value);
 }
 
 function make_url(url, key) {
@@ -20,19 +23,28 @@ function compare(a,b) {
   return 0;
 }
 
-function addLinks() {
-	chrome.extension.sendRequest({method: "getLocalStorage", key: "linkList"}, function(response) {
-  	var linkList = JSON.parse(response.data);
+
+  	
+
+function addLinks() {	
   	linkList.sort(compare);
   	var key = getKeywords();
   	for(var i=0; i<linkList.length; i++) {
   		if(linkList[i].active) {addLink(linkList[i].facetext, linkList[i].pos, linkList[i].url, key); }
   	}
-});	
-
 }
 
-
+function removeLinks() {
+	var topbar=document.getElementById('gbzc'); 
+	if (topbar == null) return;
+	
+	for(var i=1; i<=10; i++) {
+		if(document.getElementById("clinkpos"+i)) {
+			var linkbox = document.getElementById("clinkpos"+i);
+			if(linkbox) topbar.removeChild(linkbox);
+		}
+	}
+}
 
 function addLink(text,pos,url,key) {
   var topbar=document.getElementById('gbzc');  
@@ -62,11 +74,20 @@ function addLink(text,pos,url,key) {
   link.onmouseout=function() {this.className='gbzt';};
   link.appendChild(tspaceSpan);
   link.appendChild(linkTextSpan);
-  
+
   linkbox.appendChild(link);
-  
+  linkbox.setAttribute('id', "clinkpos"+pos);  
   topbar.insertBefore(linkbox, topbar.childNodes[parseInt(pos)-1]);
 }
 
-window.onload = function () { addLinks(); }
+var linkList;
+chrome.extension.sendRequest({method: "getLocalStorage", key: "linkList"}, function(response) {
+  	linkList = JSON.parse(response.data);
+  	addLinks();
+});	
+
+gbqfq.addEventListener('change', function() {
+	removeLinks();
+	addLinks();
+});
 //addLink('Scholar',4,"https://scholar.google.com/scholar?hl=en&q=%key",getKeywords());
